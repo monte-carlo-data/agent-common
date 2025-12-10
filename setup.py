8fd51618c699a4856a58e477a146f8d667deaf68
Martin Rostan <mrostan@montecarlodata.com>
2025-12-10 08:28:05 -0300

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,44 @@
+import distutils.text_file  # type: ignore
+from pathlib import Path
+from typing import List, Optional
+
+from setuptools import setup, find_packages
+
+
+def parse_requirements(
+    file_name: Optional[str] = "requirements.in",
+) -> List[str]:
+    return distutils.text_file.TextFile(
+        filename=str(Path(__file__).with_name(file_name))  # type: ignore
+    ).readlines()
+
+
+def get_long_description():
+    with open("README.md", "r") as fh:
+        return fh.read()
+
+
+setup(
+    name="agent-common",
+    use_scm_version=True,
+    license="https://github.com/monte-carlo-data/agent-common/blob/main/LICENSE",
+    description="Monte Carlo's Agent - Common",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Monte Carlo Data, Inc",
+    author_email="info@montecarlodata.com",
+    url="https://www.montecarlodata.com/",
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    install_requires=parse_requirements(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.12",
+    setup_requires=["setuptools", "setuptools_scm"],
+)

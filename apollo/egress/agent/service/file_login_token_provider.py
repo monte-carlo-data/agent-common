6be92deea7e6230fef53038b7b43847f236360e9
Martin Rostan <mrostan@montecarlodata.com>
2025-12-24 16:00:55 -0300

    add new login token provider that loads it from a file

---
+++ apollo/egress/agent/service/file_login_token_provider.py
@@ -0,0 +1,43 @@
+import json
+import logging
+import os
+from json import JSONDecodeError
+from typing import Dict
+
+from apollo.egress.agent.service.login_token_provider import LoginTokenProvider
+from apollo.egress.agent.utils.utils import X_MCD_ID, X_MCD_TOKEN
+
+_MCD_ID_ATTR = "mcd_id"
+_MCD_TOKEN_ATTR = "mcd_token"
+_NO_TOKEN_ID = "no-token-id"
+_NO_TOKEN_SECRET = "no-token-secret"
+
+logger = logging.getLogger(__name__)
+
+
+class FileLoginTokenProvider(LoginTokenProvider):
+    def __init__(self, file_path: str):
+        self._file_path = file_path
+
+    def get_token(self) -> Dict[str, str]:
+        if os.path.exists(self._file_path):
+            with open(self._file_path, "r") as f:
+                key_str = f.read()
+            try:
+                key_json = json.loads(key_str)
+                if _MCD_ID_ATTR in key_json and _MCD_TOKEN_ATTR in key_json:
+                    return {
+                        X_MCD_ID: key_json[_MCD_ID_ATTR],
+                        X_MCD_TOKEN: key_json[_MCD_TOKEN_ATTR],
+                    }
+                else:
+                    logger.warning(f"Invalid secret string, keys: {key_json.keys()}")
+            except JSONDecodeError as ex:
+                logger.error(f"Failed to parse Key JSON: {ex}")
+        else:
+            logger.warning("No token file found")
+
+        return {
+            X_MCD_ID: _NO_TOKEN_ID,
+            X_MCD_TOKEN: _NO_TOKEN_SECRET,
+        }
